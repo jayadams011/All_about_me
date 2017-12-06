0f0d5690fe4215040c82227a257628803d48d09e
Jay Adams <j.adams011@hotmail.com>
2017-12-05 16:59:23 -0800

    Draft ready for first submission.

--- app.js
+++ app.js
@@ -28,3 +28,44 @@ var inAnswertwo = prompt(interviewersName+ ' ' +'Do you like to Travel: yes or n
         alert('You must do local vacations then.');
         console.log(inAnswertwo, interviewersName,'doesn\'t like to travel');
     }
+
+    var inAnswerthree = prompt(interviewersName+ ' ' +'have you ever been tackeled by a great dane: yes or no?');
+        if (inAnswerthree){
+            inAnswerthree = inAnswerthree.toLowerCase();
+        }
+
+        if (inAnswerthree === 'yes'){
+            alert('Then you must be very lucky!');
+            console.log(inAnswerthree,interviewersName,'has been loved by a dane');
+        }
+        if (inAnswerthree === 'no'){
+            alert('Oh, too bad. Nothing better then a Dane Hug');
+            console.log(inAnswerthree, interviewersName,'hasn\'t been loved by a dane');
+        }
+
+      var inAnswerfour = prompt(interviewersName+ ' ' +'Do you ride a bike?');
+            if (inAnswerfour){
+                inAnswerfour = inAnswerfour.toLowerCase();
+            }
+
+            if (inAnswerfour === 'yes'){
+                alert('Cool. I ride road.');
+                console.log(inAnswerfour,interviewersName,'Does ride a bike.');
+            }
+            if (inAnswerfour === 'no'){
+                alert('So, I guess you have never experinced going downhill at 60 mph with a road contact of about two quarters.');
+                console.log(inAnswerfour, interviewersName,'does\'t ride a bike');
+            }
+        var inAnswerfive = prompt(interviewersName+ ' ' +'Are you getting tired of my questions?');
+            if (inAnswerfive){
+                inAnswerfive = inAnswerfive.toLowerCase();
+            }
+
+            if (inAnswerfive === 'yes'){
+                alert('it\'s ok. We are almost done.');
+                console.log(inAnswerfive,interviewersName,'is tired of these questions');
+            }
+            if (inAnswerfive === 'no'){
+                alert('Well, that\'s all I have for now.');
+                console.log(inAnswerfive, interviewersName,'isn\'t ready to quit');
+            }
