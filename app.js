012ba6700e3b4b5689ac3ce560c258343bebab3d
Jay Adams <j.adams011@hotmail.com>
2017-12-05 16:35:45 -0800

    This is a bit better.  A little further along.

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 ' use strict';
 var interviewersName = prompt ('Hi, What is your name?');
-      console.log(The interviewersName);
+      console.log('The interviewers name is', interviewersName);
 
-var answer = prompt(interviewersName + ', this is a yes/no question.  Please answer with a yes or no.  Do you like to cook?');
-      console.log('interviewersName:', answer);
+var inAnswer = prompt(interviewersName +' '+'Do you like to cook: yes or no?');
+  if (inAnswer){
+      inAnswer = inAnswer.toLowerCase();
+  }
+  if (inAnswer === 'yes'){
+      alert('Awesome! I bet you are a foodie!');
+      console.log(inAnswer,interviewersName,'likes to cook');
+  }
+  if (inAnswer === 'no'){
+      alert('Well, I bet you still enjoy eating out with friends!');
+      console.log(inAnswer, interviewersName,'doesn\'t like to cook');
+  }
+
+var inAnswertwo = prompt(interviewersName+ ' ' +'Do you like to Travel: yes or no?');
+    if (inAnswertwo){
+        inAnswertwo = inAnswertwo.toLowerCase();
+    }
+
+    if (inAnswertwo === 'yes'){
+        alert('Thats fun.  I wonder if we have seen the same things!');
+        console.log(inAnswertwo,interviewersName,'likes to travel');
+    }
+    if (inAnswertwo === 'no'){
+        alert('You must do local vacations then.');
+        console.log(inAnswertwo, interviewersName,'doesn\'t like to travel');
+    }
