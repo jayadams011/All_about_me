6f96b86e74b84261adec3120276042a2d54dd949
Jay Adams <j.adams011@hotmail.com>
2017-12-05 16:16:58 -0800

    mostly working about me before I get into trouble

---
+++ app.js
@@ -0,0 +1,6 @@
+' use strict';
+var interviewersName = prompt ('Hi, What is your name?');
+      console.log(The interviewersName);
+
+var answer = prompt(interviewersName + ', this is a yes/no question.  Please answer with a yes or no.  Do you like to cook?');
+      console.log('interviewersName:', answer);
